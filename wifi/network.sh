fddfb5a0c4e954b2f0a3f7ad615aad582ddde846
Daniel Roulin <dadaroulin@bluewin.ch>
2022-03-12 17:19:42 +0100

    + debug lvl

--- wifi/network.sh
+++ wifi/network.sh
@@ -14,7 +14,7 @@ printf "[DEBUG:] starting virtual interface...\n"
 ifconfig hotspot 10.0.0.1
 
 printf "[DEBUG:] launching hostapd...\n"
-hostapd network.conf
+hostapd -dd network.conf
 
 printf "[DEBUG:] deleting virtual interface...\n"
 iw dev hotspot del
