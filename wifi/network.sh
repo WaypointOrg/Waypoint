12a9e774d18066d3ba6b4b256282f47dd2139628
Daniel Roulin <dadaroulin@bluewin.ch>
2022-03-13 13:40:21 +0100

    creating hostpot on wlan0

--- wifi/network.sh
+++ wifi/network.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-interface="wlp110s0"
+interface="wlan0"
 
 # printf "[DEBUG:] Updating iptables...\n"
 # echo "1" > /proc/sys/net/ipv4/ip_forward
 # iptables --table nat --append POSTROUTING --out-interface $interface -j MASQUERADE
 # iptables --append FORWARD --in-interface hotspot -j ACCEPT
 
-printf "[DEBUG:] creating virtual interface...\n"
-iw phy phy0 interface add hotspot type __ap
+# printf "[DEBUG:] creating virtual interface...\n"
+# iw phy phy0 interface add hotspot type __ap
 
-printf "[DEBUG:] starting virtual interface...\n"
-ifconfig hotspot 10.0.0.1
+# printf "[DEBUG:] starting virtual interface...\n"
+# ifconfig hotspot 10.0.0.1
 
 printf "[DEBUG:] launching hostapd...\n"
 hostapd network.conf 
 # > hostapd_output.txt
 
-printf "[DEBUG:] deleting virtual interface...\n"
-iw dev hotspot del
+# printf "[DEBUG:] deleting virtual interface...\n"
+# iw dev hotspot del
