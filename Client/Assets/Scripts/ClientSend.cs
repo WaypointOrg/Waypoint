481daf3c44a7c67012b454cf04308b7a0edffa3c
Daniel Roulin <dadaroulin@bluewin.ch>
2022-03-17 18:11:13 +0100

    Added default Unity Client

---
+++ Client/Assets/Scripts/ClientSend.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    private static void SendTCPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.instance.tcp.SendData(_packet);
+    }
+
+    private static void SendUDPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.instance.udp.SendData(_packet);
+    }
+
+    #region Packets
+    public static void WelcomeReceived()
+    {
+        using (Packet _packet = new Packet((int)ClientPackets.welcomeReceived))
+        {
+            _packet.Write(Client.instance.myId);
+            _packet.Write(UIManager.instance.usernameField.text);
+
+            SendTCPData(_packet);
+        }
+    }
+
+    public static void UDPTestReceived()
+    {
+        using (Packet _packet = new Packet((int)ClientPackets.updTestReceived))
+        {
+            _packet.Write("Received a UDP packet.");
+
+            SendUDPData(_packet);
+        }
+    }
+    #endregion
+}
