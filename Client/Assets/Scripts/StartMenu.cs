883b40d13daac326206b981b50ef3659b62eb8e5
Daniel Roulin <dadaroulin@bluewin.ch>
2022-08-29 12:49:46 +0200

    Fixed

--- Client/Assets/Scripts/StartMenu.cs
+++ Client/Assets/Scripts/StartMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     public GameObject startMenu;
 
